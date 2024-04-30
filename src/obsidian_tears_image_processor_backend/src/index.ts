62a65e17423e358f0a735ceb759ccdc7be886d86
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-04-30 17:55:26 +0100

    added express

--- src/obsidian_tears_image_processor_backend/src/index.ts
+++ src/obsidian_tears_image_processor_backend/src/index.ts
@@ -1,7 +1,16 @@
-import { Canister, query, text } from 'azle';
+import { Server } from "azle";
+import express from "express";
 
-export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
-})
+export default Server(() => {
+  const app = express();
+
+  // main function, to read from other canister
+  // parse it and return with the png transformation
+  app.get("/", (req, res) => {
+    if (!req.query.hero_id) res.send(`Query parameter "hero_id" is required.`);
+
+    res.send(`<h1>${req.query.hero_id}</h1>`);
+  });
+
+  return app.listen();
+});
